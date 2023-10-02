14226be0ca6abff03aa35146855ba87d375b2db2
Veena <veena1695@gmail.com>
2023-10-02 13:47:28 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sudoku';
+  matrix = [
+    [{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false}],
+    [{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false}],
+    [{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false}],
+    [{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false}],
+    [{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false}],
+    [{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false}],
+    [{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false}],
+    [{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false}],
+    [{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false},{val : ' ', isInitial: false}],
+  ];
+  columns = [0,1,2,3,4,5,6,7,8]
+  rows = [0,1,2,3,4,5,6,7,8];
+  isSubmitted = false;
+
+
+  handleKeyPress(evt: KeyboardEvent, row:number, col:number){
+    if(evt.keyCode < 48 || evt.keyCode > 57){
+      console.log(evt)
+      evt.preventDefault();
+    }else{
+      this.matrix[row][col].val = evt.key;
+    }
+    if(this.isSubmitted) this.checkIfGridValid()
+  }
+
+  checkIfGridValid(){
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
